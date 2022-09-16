ff7b6d883769cfa0d7597233ea69717a709b4275
Mkparu Somtochi <mkparusomtochi26@gmail.com>
2022-09-17 00:10:46 +0100

    Dynamic values

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import './App.css';
+
 export default function App(){
+const title = 'Welcome to my superhero blog';
+const bool = true;
+const numbers =[ 1,2,3,4,5];
+const link ='http://www.google.com';
     return (
         <div className="App">
-            <h1>My first React App</h1>
-            <p>this is my first app</p>
+            <h1> {title} </h1>
+            {bool && <p>I was printed</p>}
+            <p>{numbers}</p>
+            <a href ={link}  target='_blank' >Google.com</a>
         </div>
     );
 }
