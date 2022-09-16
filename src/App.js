0b84c29ac2c6c61c01050a3da364bd2ac0b88aeb
Mkparu Somtochi <mkparusomtochi26@gmail.com>
2022-09-16 23:46:24 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+export default function App(){
+    return (
+        <div className="App">
+            <h1>My first React App</h1>
+            <p>this is my first app</p>
+        </div>
+    );
+}
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+ import React from 'react';
+import ReactDOM  from 'react-dom'; 
+import App from '../src/App';
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+    <App/>
+    </>
+);
