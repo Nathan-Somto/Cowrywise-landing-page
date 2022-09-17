4d23b032b8beb6125d36f68d8c73de0bfc411048
Mkparu Somtochi <mkparusomtochi26@gmail.com>
2022-09-17 02:20:53 +0100

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,17 @@
 export default function Home ()
 {
+    const handleClick = () =>{
+        console.log(" i have been clicked");
+    }
+    const createNameList = (name ,e)=>{
+         console.log(`<li>${name}, ${e.target}</li>`);
+    }
     return(
-        <h1>Welcome to my superhero Blog</h1>
+        <div className ="Home">
+        <h1>Welcome to my Superhero Blog</h1>
+        <p>Want to learn more about superheroes</p>
+        <button onClick ={handleClick}>Click me</button>
+        <button onClick = { (e) => (createNameList('Batman',e))}>Get Batman</button>
+        </div>
     );
 }
